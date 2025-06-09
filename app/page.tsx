450e03afd8661f0658beedcd9f7fa1e14a4971b5
alexabreup <eletrolabelt@gmail.com>
2025-06-09 20:45:10 -0300

    Volta a usar imagens locais para os logos do SENAI

--- app/page.tsx
+++ app/page.tsx
@@ -369,14 +369,14 @@ export default function Home() {
             </div>
             <div className="flex items-center gap-4 mt-2">
               <img
-                src="https://logodownload.org/wp-content/uploads/2019/08/senai-logo-1.png"
+                src="/SENAI_Sao_Paulo_logo.png"
                 alt="SENAI São Paulo Logo"
                 width={80}
                 height={30}
                 style={{ objectFit: 'contain' }}
               />
               <img
-                src="https://www.fieb.org.br/midia/2019/5/senai-logo.png"
+                src="/senai-saga-logo-dark-new.png"
                 alt="SENAI SAGA Logo"
                 width={80}
                 height={30}
